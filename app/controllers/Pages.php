4bd5b6618344d344ab481dc4f6617dd5eacfa70e
Peter Ahlgren <ahlgren1234@gmail.com>
2021-04-01 12:47:01 +0200

    Include header and footer

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -16,6 +16,9 @@ class Pages extends Controller
 
 
     public function about() {
-        $this->view('pages/about');
+        $data = [
+            'title' => 'About Us'
+        ];
+        $this->view('pages/about', $data);
     }
 }
